2496d5fa8a85fc808288d19a9b7f68be6104289d
sys-anirban <anirbanghatak92@gmail.com>
2021-05-20 12:07:02 +0530

    added async

--- controller/user.js
+++ controller/user.js
@@ -4,32 +4,31 @@ const jwt = require('jsonwebtoken');
 const { ProblemError } = require('../middleware/error');
 const errorDescription = require('../constants/errors');
 
-const login = (req, res, next) => {
-  const { username, password } = req.body;
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    ProblemError(errorDescription.VALIDATION_ERROR, 422, { isInvalid: true });
+const login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      ProblemError(errorDescription.VALIDATION_ERROR, 422, { isInvalid: true });
+    }
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      ProblemError(errorDescription.USER_NOT_FOUND, 401, { isInvalid: true });
+    } else if (user.password !== password) {
+      ProblemError(errorDescription.INVALID_CREDENTIALS, 401, {
+        isInvalid: true,
+      });
+    }
+    const token = jwt.sign(
+      {
+        username: user.username,
+      },
+      'token',
+      { expiresIn: '1h' }
+    );
+    res.status(200).json({ token, isUserAuthenticated: true });
+  } catch (error) {
+    next(error);
   }
-  User.findOne({ username: username })
-    .then((user) => {
-      if (!user) {
-        ProblemError(errorDescription.USER_NOT_FOUND, 401, { isInvalid: true });
-      } else if (user.password !== password) {
-        ProblemError(errorDescription.INVALID_CREDENTIALS, 401, {
-          isInvalid: true,
-        });
-      }
-      const token = jwt.sign(
-        {
-          username: user.username,
-        },
-        'token',
-        { expiresIn: '1h' }
-      );
-      res.status(200).json({ token, isUserAuthenticated: true });
-    })
-    .catch((error) => {
-      next(error);
-    });
 };
 module.exports = { login };
